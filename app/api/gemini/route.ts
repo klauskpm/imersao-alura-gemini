186ecc1c7637fd419615647daac1cf54d1dfc463
Klaus Kazlauskas <klausmachado@gmail.com>
2024-09-02 19:18:27 -0400

    move AI model to the request

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -3,14 +3,9 @@ const {
 } = require("@google/generative-ai");
 
 const apiKey = process.env.GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
-});
 
 const generationConfig = {
-    temperature: 1,
+    temperature: 0.5,
     topP: 0.95,
     topK: 64,
     maxOutputTokens: 8192,
@@ -18,16 +13,19 @@ const generationConfig = {
 };
 
 export async function GET(request: Request) {
+    const genAI = new GoogleGenerativeAI(apiKey);
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-pro",
+    });
+
     const chatSession = model.startChat({
-    generationConfig,
-    // safetySettings: Adjust safety settings
-    // See https://ai.google.dev/gemini-api/docs/safety-settings
-    history: [
-    ],
+        generationConfig,
+        // safetySettings: Adjust safety settings
+        // See https://ai.google.dev/gemini-api/docs/safety-settings
+        history: [],
     });
 
     const result = await chatSession.sendMessage("Create a poem about pokemon.");
-    console.log(result.response.text());
 
     return new Response(result.response.text());
 }
