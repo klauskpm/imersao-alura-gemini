af83c07b3e3a1fb55fdd9e9ec100e48f3e1b0fcf
Klaus Kazlauskas <klausmachado@gmail.com>
2024-09-02 18:02:41 -0400

    add postgres, neon and drizzle to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-export default function Home() {
+import { db } from "@/drizzle/db";
+import { sql } from "@vercel/postgres";
+
+export const getUsers = async () => {
+  return db.query.users.findMany();
+};
+
+export default async function Home() {
+  const users = await getUsers();
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
