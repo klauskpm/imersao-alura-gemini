3a4607d17ac4d5e761bd0975f16226b6630c1e40
Klaus Kazlauskas <klausmachado@gmail.com>
2024-09-22 09:32:03 -0400

    Remove AI components from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { db } from "@/drizzle/db";
-import AiClientResponse from "./ai-client-response";
-import AiServerResponse from "./ai-server-response";
 
 const getUsers = async () => {
   return db.query.users.findMany();
@@ -11,8 +9,6 @@ export default async function Home({ searchParams }: { searchParams: { text?: st
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AiClientResponse text={searchParams.text} />
-      <AiServerResponse text={searchParams.text} />
       <h1 className="text-4xl font-bold">Users</h1>
       <ul>
         {users.map((user) => (
